127d66f26fdaa6f053427d3c6ba3c648fdc4def5
Berat UNLUCE <beratunluce@hotmail.com>
2022-08-11 15:20:14 +0300

    static files

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
-const fn = function () {
-  console.log('Hello World');
-};
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname + '/temp/index.html'));
+});
+
+const port = 3000;
+app.listen(port, () => console.log(`App listening on port ${port}!`));
