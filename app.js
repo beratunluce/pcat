cd75cb1a92b274f527652f7ee6417b9baae36b9e
Berat UNLUCE <beratunluce@hotmail.com>
2022-08-15 20:56:25 +0300

    updated

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const app = express();
+const ejs = require('ejs');
 const path = require('path');
 
+/// template engine
+app.set('view engine', 'ejs');
+
+/// middlewares
 app.use(express.static('public'));
 
+/// routes
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname + '/temp/index.html'));
+  // res.sendFile(path.resolve(__dirname + '/temp/index.html'));
+  res.render('index');
+});
+app.get('/add', (req, res) => {
+  // res.sendFile(path.resolve(__dirname + '/temp/index.html'));
+  res.render('add');
+});
+app.get('/about', (req, res) => {
+  // res.sendFile(path.resolve(__dirname + '/temp/index.html'));
+  res.render('about');
 });
 
 const port = 3000;
