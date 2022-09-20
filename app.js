2b5e7df08fe689eaf5e9418c6805d00daf244701
Berat UNLUCE <beratunluce@hotmail.com>
2022-09-20 22:26:30 +0300

    posts

--- app.js
+++ app.js
@@ -1,26 +1,45 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 const ejs = require('ejs');
 const path = require('path');
+const Photo = require('./models/Photo');
+
+//mongodb connect
+mongoose.connect('mongodb://localhost/pcat-test-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 /// template engine
 app.set('view engine', 'ejs');
 
 /// middlewares
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+app.use(express.json());
 
 /// routes
-app.get('/', (req, res) => {
-  // res.sendFile(path.resolve(__dirname + '/temp/index.html'));
+app.get('/', async (req, res) => {
+  const photos = await Photo.find({});
+  res.render('index', {
+    photos,
+  });
+});
+
+app.get('/index', (req, res) => {
   res.render('index');
 });
+
 app.get('/add', (req, res) => {
-  // res.sendFile(path.resolve(__dirname + '/temp/index.html'));
   res.render('add');
 });
 app.get('/about', (req, res) => {
-  // res.sendFile(path.resolve(__dirname + '/temp/index.html'));
   res.render('about');
+});
+app.post('/photos', async (req, res) => {
+  await Photo.create(req.body);
+  res.redirect('/');
 });
 
 const port = 3000;
